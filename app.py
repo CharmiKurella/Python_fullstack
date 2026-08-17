c1a2de5f80fefc5ea795865a59c6fbd30a9eaf0b
CharmiKurella <charmikurella@gmail.com>
2026-08-17 10:25:44 +0000

    upto app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask,render_template,jsonify,request,redirect,url_for
+from flask import Flask,render_template,jsonify,request,redirect,url_for,session
 
 
 
@@ -88,9 +88,16 @@ def api_login():
     user = cursor.fetchone()
     conn.close()
     if user and user["password"] == password:
+        session["user_email"] = user["email"]
+        session["user_name"] = user["name"]
         return jsonify({"status": "success", "message": "login successful! Welcome back."})
     else:
         return jsonify({"status": "error", "message": "Invalid email or password!"}), 401
+app.route('/logout', methods=["GET"])
+def logout():
+    session.app("user_email", None)
+    session.app("user_name", None)
+    return redirect(url_for("login"))
 
 if __name__=='__main__':
     app.run(debug=True)
